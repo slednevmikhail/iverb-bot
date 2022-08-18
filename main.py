6106265110903a4caaca8d9849e70bc05e25237b
yeb1ch <89690018+yeb1ch@users.noreply.github.com>
2022-08-18 13:13:25 +0500

    fukdat

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-﻿import re
+import discord
+from discord.ext import commands
+
+import re
 
 import requests  # установи
 import pickle  # дефолтная хуйня устанавливать не надо
@@ -83,10 +86,42 @@ def get_map_link(map_name):
     return map_dict[map_name.lower()]
     pass
 
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=['iverb ', 'иверб '])
+client = discord.client
 
 
-def Test(): #ИДИ НАХУЙ
-    #print(is_exists('228123'))
-    #print(is_exists('тюбик'))
-    #print(is_exists('76561198015267733'))
-    get_map_players('Core')
+@bot.command()
+async def скор(ctx, *map_name):
+    map_name_string = ' '.join(map_name)
+    map_players = get_map_players(map_name_string)
+    formatted_string = format_to_string(map_players, map_name_string)
+    return await ctx.send(formatted_string)
+
+
+def format_to_string(map_players_list, map_name):
+    result_string = f'```Scores on map {map_name}:\n\n'
+    for player in map_players_list:
+        result_string += f'{player.rank}. {player.name} - {player.time}\n'
+    return result_string + '\n```'
+
+
+@bot.command()
+async def добавить(ctx, player_id):
+    if add_player(player_id):
+        return await ctx.send('Игрок добавлен')
+    return await ctx.send('Иди нахуй')
+
+
+@bot.command()
+async def убрать(ctx, player_id):
+    remove_player(player_id)
+    return await ctx.send('Игрок убран')
+
+
+@bot.event
+async def on_ready():
+    print('My Ready is Body')
+
+
+bot.run('MTAwODQyNDc3MjQwNjg4NjQ3Mw.G6Mx_t.zq9LLlJht8lMqndTDhEWa6a4f0q3PE7D7_uxWs')
