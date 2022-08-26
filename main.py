e0899a54a9093cd65dc5f326e855635c5cdf4314
yeb1ch <89690018+yeb1ch@users.noreply.github.com>
2022-08-26 23:25:49 +0500

    wr command

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ map_filehandler = open('maps_list.pickle', 'rb')
 map_dict = pickle.load(map_filehandler)
 map_filehandler.close()
 
+
 def load_players():
     read_filehandler = open('players.pickle', 'rb')
     player_id_list = pickle.load(read_filehandler)
@@ -34,13 +35,34 @@ def is_exists(iverb_id):
     player_page = requests.get(f'https://board.portal2.sr/profile/{iverb_id}')
     page_soup = BeautifulSoup(player_page.text, 'lxml')
     nickname = page_soup.find('div', class_='nickname')
-    return nickname!= None and nickname.text != ''
+    return nickname != None and nickname.text != ''
 
 
 def remove_player(player_id):
     load_players()
     player_id_list.remove(player_id)
     save_players()
+
+
+class Run_info:
+    def __init__(self, player_name, time, demo_link, autorender_link):
+        self.player_name = player_name
+        self.time = time
+        self.demo_link = demo_link
+        self.autorender_link = autorender_link
+
+
+def get_wr_info(map_name):
+    map_link = get_map_link(map_name)
+    page_soup = get_soup(map_link)
+    first_place_entry = page_soup.find(class_='entry')
+    player_name = first_place_entry.find(class_='boardname').text
+    player_time = first_place_entry.find(class_='score').text
+    demo_href = first_place_entry.find(class_='demo-url').a.get('href')
+    demo_link = f'https://board.portal2.sr{demo_href}'
+    autorender_link = f'https://autorender.portal2.sr/video.html?v={demo_link[-6:]}'
+    return Run_info(player_name, player_time, demo_link, autorender_link)
+    pass
 
 
 class Player:
@@ -71,8 +93,7 @@ def save_players():  # это не суй в бота
 
 def get_map_players(map_name):
     map_link = get_map_link(map_name)
-    map_page = requests.get(f'https://board.portal2.sr{map_link}')
-    page_soup = BeautifulSoup(map_page.text, 'lxml')
+    page_soup = get_soup(map_link)
     result_list = []
     for player_id in player_id_list:
         player = get_results(player_id, page_soup)
@@ -85,6 +106,21 @@ def get_map_players(map_name):
 def get_map_link(map_name):
     return map_dict[map_name.lower()]
     pass
+
+
+def get_soup(map_link):
+    map_page = requests.get(f'https://board.portal2.sr{map_link}')
+    page_soup = BeautifulSoup(map_page.text, 'lxml')
+    return page_soup
+
+
+def formatted_run_info(map_name):
+    wr_info = get_wr_info(map_name)
+    print(f'``` WR on map {map_name}: '
+          f'\n {wr_info.player_name} - f{wr_info.time} '
+          f'\n demo - {wr_info.demo_link} '
+          f'\n autorender - {wr_info.autorender_link}```')
+
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=['iverb ', 'иверб '], intents=intents)
@@ -110,7 +146,7 @@ def format_to_string(map_players_list, map_name):
 async def добавить(ctx, player_id):
     if add_player(player_id):
         return await ctx.send('Игрок добавлен')
-    return await ctx.send('Иди нахуй')
+        return await ctx.send('Иди нахуй')
 
 
 @bot.command()
@@ -118,6 +154,11 @@ async def убрать(ctx, player_id):
     remove_player(player_id)
     return await ctx.send('Игрок убран')
 
+
+@bot.command()
+async def вр(ctx, *map_name):
+    map_name_string = ' '.join(map_name)
+    return await ctx.send(formatted_run_info(map_name))
 
 @bot.event
 async def on_ready():
